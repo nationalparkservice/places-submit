96d9531b106b2ec06e4836ab5d0fde3fae2b9e65
Nate Irwin <nate@nateirwin.net>
2015-11-19 15:14:46 -0700

    Lock down submits for "stewarded" parks

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 
 // TODO: Loading bar should update for three different phases of submit.
 // TODO: If not in 'iframe' mode, update URL with parameters as changes to points are made.
+// TODO: Switch modal auth window to a slidedown panel.
 
 var iD = {
   data: {}
@@ -104,14 +105,156 @@ $(document).ready(function () {
     $('#backdrop').hide();
     $('#loading').hide();
   }
+  function init () {
+    auth = osmAuth({
+      modal: true,
+      oauth_consumer_key: 'F7zPYlVCqE2BUH9Hr4SsWZSOnrKjpug1EgqkbsSb',
+      oauth_secret: 'rIkjpPcBNkMQxrqzcOvOC4RRuYupYr7k8mfP13H5',
+      url: 'http://10.147.153.193' + (dev ? ':8000' : '')
+    });
+
+    if (iframe) {
+      $cancel.on('click', function () {
+        window.parent.window.postMessage('cancel', '*');
+      });
+    } else {
+      $cancel.remove();
+    }
+
+    NPMap = {
+      baseLayers: [
+        'nps-parkTilesImagery'
+      ],
+      center: center,
+      closePopupOnClick: false,
+      div: 'map',
+      geocoderControl: true,
+      hashControl: (iframe ? undefined : true),
+      hooks: {
+        init: function (callback) {
+          map = NPMap.config.L;
+
+          if (dev) {
+            $('.leaflet-control-attribution')[0].innerHTML += ' | <span style="color:#d9534f;">DEV</span>';
+          }
+
+          if (editId && editLatLng) {
+            $drop.hide();
+            marker = new L.Marker(editLatLng, {
+              draggable: true,
+              icon: L.npmap.icon.maki({
+                'marker-color': '#d95f02'
+              })
+            }).addTo(map);
+            map.setView(editLatLng, 18);
+            toSecondEditStep();
+          } else {
+            $del.hide();
+            $submit.hide();
+          }
+
+          $buttons.show();
+          L.npmap.util._.appendJsFile('https://nationalparkservice.github.io/places-data/npmapPresets.js');
+          callback();
+        }
+      },
+      scrollWheelZoom: (iframe ? undefined : true),
+      zoom: zoom
+    };
+    window.addEventListener('message', function receiveMessage (e) {
+      if (e.origin !== 'http://insidemaps.nps.gov') {
+        return;
+      } else {
+        authComplete(e.data);
+      }
+    }, false);
+    s.src = 'http://www.nps.gov/lib/npmap.js/3.0.7/npmap-bootstrap.min.js';
+    document.body.appendChild(s);
+    $('#locked .btn-default').click(function () {
+      $('#locked').hide();
+      $('#backdrop').hide();
+    });
+    $del.click(function () {
+      var $btn = $(this);
+
+      $btn
+        .popover({
+          animation: false,
+          container: 'body',
+          content: '' +
+            '<div class="confirm-delete">' +
+              '<p>Are you sure you want to delete <em>"' + name + '"</em>?</p>' +
+              '<div style="text-align:center;">' +
+                '<button class="btn btn-default" style="margin-right:5px;" type="button">Cancel</button>' +
+                '<button class="btn btn-danger" type="button">Confirm</button>' +
+              '</div>' +
+            '</div>' +
+          '',
+          html: true,
+          placement: 'top',
+          trigger: 'manual'
+        })
+        .on('hidden.bs.popover', function () {
+          $('#backdrop')
+            .css('z-index', '1040')
+            .hide();
+        })
+        .on('shown.bs.popover', function () {
+          $('#backdrop')
+            .css('z-index', '1059')
+            .show();
+          $('.confirm-delete .btn-danger').click(function () {
+            $btn.popover('destroy');
+            verifyAuth(function () {
+              showLoading();
+              submit(function (result) {
+                if (result.success) {
+                  if (iframe) {
+                    window.parent.window.postMessage('delete:' + editId, '*');
+                  } else {
+                    map.notify.success('Node: ' + editId + ' deleted!');
+                    map.removeLayer(marker);
+                    $buttons.remove();
+                  }
+                } else {
+                  map.notify.danger('The node could not be deleted. Please try again.');
+                }
+
+                hideLoading();
+              }, editId, true);
+            });
+          });
+          $('.confirm-delete .btn-default').click(function () {
+            $btn.popover('destroy');
+          });
+        });
+      $btn.popover('show');
+    });
+    $drop.click(function () {
+      marker = new L.Marker(map.getCenter(), {
+        draggable: true,
+        icon: L.npmap.icon.maki({
+          'marker-color': '#d95f02'
+        })
+      }).addTo(map);
+      toSecondEditStep();
+    });
+    $submit.on('click', function () {
+      save();
+
+      return false;
+    });
+  }
   function save () {
     if (!saving) {
+      var $prev = $name.prev();
+
       if (!name || !name.length) {
-        $name.prev().css('color', '#a94442');
+        $prev.css('color', '#a94442');
       } else {
-        saving = true;
-        $name.prev().css('color', '#464646');
+        $prev.css('color', '#464646');
         verifyAuth(function () {
+          saving = true;
           showLoading();
           submit(function (result) {
             if (result.success && result.upload && result.upload.result && result.upload.result.childNodes && result.upload.result.childNodes[0]) {
@@ -290,138 +433,48 @@ $(document).ready(function () {
     });
   }
 
-  $del.click(function () {
-    var $btn = $(this);
+  $.getJSON('https://nps.cartodb.com/api/v2/sql?q=SELECT full_name,places_lock,steward_email,steward_name FROM parks_places_status WHERE unit_code=\'' + unitCode + '\'', function (response) {
+    if (response && response.total_rows) {
+      response = response.rows[0];
+      $('#locked-park-name').html(response.full_name);
 
-    $btn
-      .popover({
-        animation: false,
-        container: 'body',
-        content: '' +
-          '<div class="confirm-delete">' +
-            '<p>Are you sure you want to delete <em>"' + name + '"</em>?</p>' +
-            '<div style="text-align:center;">' +
-              '<button class="btn btn-default" style="margin-right:5px;" type="button">Cancel</button>' +
-              '<button class="btn btn-danger" type="button">Confirm</button>' +
-            '</div>' +
-          '</div>' +
-        '',
-        html: true,
-        placement: 'top',
-        trigger: 'manual'
-      })
-      .on('hidden.bs.popover', function () {
-        $('#backdrop')
-          .css('z-index', '1040')
-          .hide();
-      })
-      .on('shown.bs.popover', function () {
-        $('#backdrop')
-          .css('z-index', '1059')
-          .show();
-        $('.confirm-delete .btn-danger').click(function () {
-          $btn.popover('destroy');
-          verifyAuth(function () {
-            showLoading();
-            submit(function (result) {
-              if (result.success) {
-                if (iframe) {
-                  window.parent.window.postMessage('delete:' + editId, '*');
-                } else {
-                  map.notify.success('Node: ' + editId + ' deleted!');
-                  map.removeLayer(marker);
-                  $buttons.remove();
-                }
-              } else {
-                map.notify.danger('The node could not be deleted. Please try again.');
+      if (response.places_lock) {
+        NPMap = {
+          baseLayers: [
+            'nps-parkTilesImagery'
+          ],
+          center: center,
+          div: 'map',
+          hooks: {
+            init: function (callback) {
+              if (dev) {
+                $('.leaflet-control-attribution')[0].innerHTML += ' | <span style="color:#d9534f;">DEV</span>';
               }
 
-              hideLoading();
-            }, editId, true);
-          });
-        });
-        $('.confirm-delete .btn-default').click(function () {
-          $btn.popover('destroy');
-        });
-      });
-    $btn.popover('show');
-  });
-  $drop.on('click', function () {
-    marker = new L.Marker(map.getCenter(), {
-      draggable: true,
-      icon: L.npmap.icon.maki({
-        'marker-color': '#d95f02'
-      })
-    }).addTo(map);
-    toSecondEditStep();
-  });
-  $submit.on('click', function () {
-    save();
+              if (iframe) {
+                $('#locked .btn-default')
+                  .click(function () {
+                    window.parent.window.postMessage('cancel', '*');
+                  })
+                  .show();
+              }
 
-    return false;
-  });
-  auth = osmAuth({
-    modal: true,
-    oauth_consumer_key: 'F7zPYlVCqE2BUH9Hr4SsWZSOnrKjpug1EgqkbsSb',
-    oauth_secret: 'rIkjpPcBNkMQxrqzcOvOC4RRuYupYr7k8mfP13H5',
-    url: 'http://10.147.153.193' + (dev ? ':8000' : '')
-  });
-
-  if (iframe) {
-    $cancel.on('click', function () {
-      window.parent.window.postMessage('cancel', '*');
-    });
-  } else {
-    $cancel.remove();
-  }
-
-  NPMap = {
-    baseLayers: [
-      'nps-parkTilesImagery'
-    ],
-    center: center,
-    closePopupOnClick: false,
-    div: 'map',
-    geocoderControl: true,
-    hashControl: (iframe ? undefined : true),
-    hooks: {
-      init: function (callback) {
-        map = NPMap.config.L;
-
-        if (dev) {
-          $('.leaflet-control-attribution')[0].innerHTML += ' | <span style="color:#d9534f;">DEV</span>';
-        }
-
-        if (editId && editLatLng) {
-          $drop.hide();
-          marker = new L.Marker(editLatLng, {
-            draggable: true,
-            icon: L.npmap.icon.maki({
-              'marker-color': '#d95f02'
-            })
-          }).addTo(map);
-          map.setView(editLatLng, 18);
-          toSecondEditStep();
-        } else {
-          $del.hide();
-          $submit.hide();
-        }
-
-        $buttons.show();
-        L.npmap.util._.appendJsFile('https://nationalparkservice.github.io/places-data/npmapPresets.js');
-        callback();
+              $('#backdrop').show();
+              $('#locked').show();
+              $('#locked .btn-default').click(function () {
+                window.parent.window.postMessage('cancel', '*');
+              });
+              $('#locked .btn-primary').attr('href', 'mailto:' + response.steward_email + '?cc=npmap@nps.gov&subject=Places Feedback for ' + unitCode.toUpperCase());
+              callback();
+            }
+          },
+          zoom: zoom
+        };
+        s.src = 'http://www.nps.gov/lib/npmap.js/3.0.7/npmap-bootstrap.min.js';
+        document.body.appendChild(s);
+      } else {
+        init();
       }
-    },
-    scrollWheelZoom: (iframe ? undefined : true),
-    zoom: zoom
-  };
-  window.addEventListener('message', function receiveMessage (e) {
-    if (e.origin !== 'http://insidemaps.nps.gov') {
-      return;
-    } else {
-      authComplete(e.data);
     }
-  }, false);
-  s.src = 'http://www.nps.gov/lib/npmap.js/2.0.1/npmap-bootstrap.min.js';
-  document.body.appendChild(s);
+  });
 });
